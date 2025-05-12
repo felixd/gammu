c888be7ff8aa7b7267fd4681327e223718701b5b
Pawe Wojciechowski <felixd@konopnickiej.com>
2025-05-12 17:43:21 +0200

    na pałę

--- libgammu/phone/at/at-sms.c
+++ libgammu/phone/at/at-sms.c
@@ -419,7 +419,9 @@ GSM_Error ATGEN_SetRequestedSMSMemory(GSM_StateMachine *s, GSM_MemoryType memory
 		return ERR_NONE;
 	}
 
-	snprintf(command, 20, "AT+CPMS=\"%s\"\r", GSM_MemoryTypeToString(memoryType));
+	// TODO: NA PAŁĘ
+	// snprintf(command, 20, "AT+CPMS=\"%s\"\r", GSM_MemoryTypeToString(memoryType));
+	snprintf(command, 20, "AT+CPMS=\"%s\",\"%s\",\"%s\"\r", GSM_MemoryTypeToString(memoryType));
 	if (writeable) {
 		// if it's writeable we assume it's also readable
 		snprintf(command + 12, 8, ",\"%s\"\r", GSM_MemoryTypeToString(memoryType));
@@ -1609,10 +1611,10 @@ GSM_Error ATGEN_GetSMSStatus(GSM_StateMachine *s, GSM_SMSMemoryStatus *status)
 
 		if (Priv->PhoneSaveSMS == AT_AVAILABLE) {
 			if (Priv->MotorolaSMS) {
-				error = ATGEN_WaitForAutoLen(s, "AT+CPMS=\"MT\"\r", 0x00, 200, ID_GetSMSStatus);
+				error = ATGEN_WaitForAutoLen(s, "AT+CPMS=\"ME\",\"ME\",\"ME\"\r", 0x00, 200, ID_GetSMSStatus);
 				Priv->SMSMemoryWrite = FALSE;
 			} else {
-				error = ATGEN_WaitForAutoLen(s, "AT+CPMS=\"ME\",\"ME\",\"ME\"\r", 0x00, 200, ID_GetSMSStatus);
+				error = ATGEN_WaitForAutoLen(s, "AT+CPMS=\"MT\"\r", 0x00, 200, ID_GetSMSStatus);
 				Priv->SMSMemoryWrite = TRUE;
 			}
 		} else {
